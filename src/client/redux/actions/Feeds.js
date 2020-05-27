5e5d2d2b7a7589c4b5bc29d8bb739156ba66fae3
Vipin Yadav <vipyadav1@publicisgroupe.net>
2020-05-27 19:49:27 +0530

    remove extra api call on page change. it will populate result from store if result is available

--- src/client/redux/actions/Feeds.js
+++ src/client/redux/actions/Feeds.js
@@ -5,22 +5,25 @@ import {
 } from "../constants/Feeds";
 import Service from "../api/FetchBase";
 
-export const fetchFeeds = (page) => (dispatch) => {
+export const fetchFeeds = (page) => (dispatch, getState) => {
   const pageno = !isNaN(page) ? Number(page) : 1;
-  dispatch({
-    type: GET_FEEDS_REQUEST,
-  });
-  return Service.get("/search", { page: pageno - 1 })
-    .then((result) => {
-      dispatch({
-        type: GET_FEEDS_SUCCESS,
-        payload: result,
-      });
-    })
-    .catch((err) => {
-      dispatch({
-        type: GET_FEEDS_FAILURE,
-        error: err,
-      });
+  if (!getState().feeds.feeds[pageno]) {
+    dispatch({
+      type: GET_FEEDS_REQUEST,
     });
+    return Service.get("/search", { page: pageno - 1 })
+      .then((result) => {
+        dispatch({
+          type: GET_FEEDS_SUCCESS,
+          payload: result,
+          pageno,
+        });
+      })
+      .catch((err) => {
+        dispatch({
+          type: GET_FEEDS_FAILURE,
+          error: err,
+        });
+      });
+  }
 };
